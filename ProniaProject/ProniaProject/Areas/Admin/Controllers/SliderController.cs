d5e3326e1280dc1a37a844e8cdada1cabe4b21dd
ayxan217 <ayxanrm-bp217@code.edu.az>
2024-11-26 12:05:47 +0400

    added

---
+++ ProniaProject/ProniaProject/Areas/Admin/Controllers/SliderController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProniaProject.DAL;
+using ProniaProject.Models;
+
+namespace ProniaProject.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class SliderController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public SliderController(AppDbContext context)
+        {
+             _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<Slider> sliders = await _context.Slides.ToListAsync();
+            return View();
+        }
+    }
+}
--- ProniaProject/ProniaProject/obj/Debug/net8.0/ProniaProject.AssemblyInfo.cs
+++ ProniaProject/ProniaProject/obj/Debug/net8.0/ProniaProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProniaProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3e1173ff3dcd44a95a5e2434fc41460100e9130")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d8e50240fa5f72174f68170ed6cbba92eacd32f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProniaProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProniaProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
